cdc264d4e2b6cb8d251847da99a6b9eefd365940
Kamari <kmoore971@gmail.com>
2025-02-17 08:16:58 -0600

    Added client side link for vercel requests

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://book-ms-client.vercel.app/ "],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
