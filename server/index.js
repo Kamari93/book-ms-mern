a9fb09f83f19e9bb2b54d98a416150f289ed85fc
Kamari <kmoore971@gmail.com>
2025-02-19 09:25:09 -0600

    Testing routes

--- server/index.js
+++ server/index.js
@@ -40,6 +40,11 @@ app.get("/dashboard", async (req, res) => {
 });
 
 //run server on port 5000
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server running on port ${process.env.PORT}`);
+// });
+
+const PORT = process.env.PORT || 5000; // Use dynamic port for production
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
