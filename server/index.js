1abe676091f1247e53b7492e8c264a739e92c90f
Kamari <kmoore971@gmail.com>
2025-02-19 09:10:27 -0600

    Testing routes

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ app.use(
     origin: ["https://book-ms-client.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(cookieParser());
